ab3ab30be57902eb97d07d52f555b267d7a437af
rok <37019118+rok805@users.noreply.github.com>
2021-04-16 20:32:45 +0900

    Add files via upload
    framework

---
+++ MLP/main.py
@@ -0,0 +1,61 @@
+
+import dataset
+from model import LeNet5, CustomMLP
+
+# import some packages you need here
+
+
+def train(model, trn_loader, device, criterion, optimizer):
+    """ Train function
+
+    Args:
+        model: network
+        trn_loader: torch.utils.data.DataLoader instance for training
+        device: device for computing, cpu or gpu
+        criterion: cost function
+        optimizer: optimization method, refer to torch.optim
+
+    Returns:
+        trn_loss: average loss value
+        acc: accuracy
+    """
+
+    # write your codes here
+
+    return trn_loss, acc
+
+def test(model, tst_loader, device, criterion):
+    """ Test function
+
+    Args:
+        model: network
+        tst_loader: torch.utils.data.DataLoader instance for testing
+        device: device for computing, cpu or gpu
+        criterion: cost function
+
+    Returns:
+        tst_loss: average loss value
+        acc: accuracy
+    """
+
+    # write your codes here
+
+    return tst_loss, acc
+
+
+def main():
+    """ Main function
+
+        Here, you should instantiate
+        1) Dataset objects for training and test datasets
+        2) DataLoaders for training and testing
+        3) model
+        4) optimizer: SGD with initial learning rate 0.01 and momentum 0.9
+        5) cost function: use torch.nn.CrossEntropyLoss
+
+    """
+
+    # write your codes here
+
+if __name__ == '__main__':
+    main()
---
+++ MLP/model.py
@@ -0,0 +1,39 @@
+
+import torch.nn as nn
+
+class LeNet5(nn.Module):
+    """ LeNet-5 (LeCun et al., 1998)
+
+        - For a detailed architecture, refer to the lecture note
+        - Freely choose activation functions as you want
+        - For subsampling, use max pooling with kernel_size = (2,2)
+        - Output should be a logit vector
+    """
+
+    def __init__(self):
+
+        # write your codes here
+
+    def forward(self, img):
+
+        # write your codes here
+
+        return output
+
+
+class CustomMLP(nn.Module):
+    """ Your custom MLP model
+
+        - Note that the number of model parameters should be about the same
+          with LeNet-5
+    """
+
+    def __init__(self):
+
+        # write your codes here
+
+    def forward(self, img):
+
+        # write your codes here
+
+        return output
